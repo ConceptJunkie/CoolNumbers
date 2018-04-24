87b9c8d84095ea7dcf139906c2be946227f6d98a
Rick Gutleber <rickg@his.com>
2018-04-24 12:25:05 -0400

    lucas factors up to 250

--- makefib.bat
+++ makefib.bat
@@ -1,5 +1,5 @@
 rpn -a100 -E 1 300 range fibonacci factor 1 enumerate -s1 > fibonacci_factors.txt
-rpn -a100 -E 1 240 range lucas factor 1 enumerate -s1 > lucas_factors.txt
+rpn -a100 -E 1 250 range lucas factor 1 enumerate -s1 > lucas_factors.txt
 rpn -a100 -E 1 200 range tribonacci factor 1 enumerate -s1 > tribonacci_factors.txt
 rpn -a100 -E 1 200 range tetranacci factor 1 enumerate -s1 > tetranacci_factors.txt
 rpn -a100 -E 1 200 range pentanacci factor 1 enumerate -s1 > pentanacci_factors.txt
