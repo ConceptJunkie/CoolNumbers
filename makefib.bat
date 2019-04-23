595afa2e2a86aab81adcd2fdaf203e232790c247
Rick Gutleber <rickg@his.com>
2019-04-23 08:21:41 -0400

    The first 500 Lucas numbers factored

--- makefib.bat
+++ makefib.bat
@@ -1,5 +1,5 @@
 rpn -a150 -E 1 600 range fibonacci factor 1 enumerate -s1 > fibonacci_factors.txt
-rpn -a150 -E 1 400 range lucas factor 1 enumerate -s1 > lucas_factors.txt
-rpn -a150 -E 1 400 range tribonacci factor 1 enumerate -s1 > tribonacci_factors.txt
-rpn -a150 -E 1 400 range tetranacci factor 1 enumerate -s1 > tetranacci_factors.txt
-rpn -a150 -E 1 400 range pentanacci factor 1 enumerate -s1 > pentanacci_factors.txt
+rpn -a150 -E 1 500 range lucas factor 1 enumerate -s1 > lucas_factors.txt
+rpn -a150 -E 1 500 range tribonacci factor 1 enumerate -s1 > tribonacci_factors.txt
+rpn -a150 -E 1 500 range tetranacci factor 1 enumerate -s1 > tetranacci_factors.txt
+rpn -a150 -E 1 500 range pentanacci factor 1 enumerate -s1 > pentanacci_factors.txt
