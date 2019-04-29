f2021e65c0b15c450c1ccad579783c6264d1c3c6
Rick Gutleber <rickg@his.com>
2019-04-29 11:50:04 -0400

    tribonacci numbers factored up to 450

--- makerepunit.bat
+++ makerepunit.bat
@@ -1,2 +1,2 @@
-rpn -a602 -E 1 8 range lambda 1 600 range 10 repunit x * next_prime eval -s1 flatten sort > repunit_next.txt
-rpn -a602 -E 1 9 range lambda 2 600 range 10 repunit x * previous_prime eval -s1 flatten sort > repunit_prev.txt
+rpn -a652 -E 1 8 range lambda 1 650 range 10 repunit x * next_prime eval -s1 flatten sort > repunit_next.txt
+rpn -a652 -E 1 9 range lambda 2 650 range 10 repunit x * previous_prime eval -s1 flatten sort > repunit_prev.txt
