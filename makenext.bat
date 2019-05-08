02b37933f05ac7f0282e7e1cb6d0979152eb5124
Rick Gutleber <rickg@his.com>
2019-05-07 21:37:09 -0400

    powers of 2 next primes up to 5000

--- makenext.bat
+++ makenext.bat
@@ -1,2 +1,2 @@
-rpn -E -a2502 1 1025 range lambda 2 x ** next_prime 2 x ** - eval 1 enumerate -s1 > powers_of_2_next_primes.txt
-rpn -E -a5010 1 2025 range lambda 10 x ** next_prime 10 x ** - eval 1 enumerate -s1 > powers_of_10_next_primes.txt
+rpn -E -a3010 1 5000 range lambda 2 x ** next_prime 2 x ** - eval 1 enumerate -s1 > powers_of_2_next_primes.txt
+rpn -E -a5010 1 5000 range lambda 10 x ** next_prime 10 x ** - eval 1 enumerate -s1 > powers_of_10_next_primes.txt
