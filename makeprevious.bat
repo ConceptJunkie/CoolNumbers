61838df6d33835fa3de3da9d1f03cdeaef7018f7
Rick Gutleber <rickg@his.com>
2019-05-29 18:42:42 -0400

    more previous primes

--- makeprevious.bat
+++ makeprevious.bat
@@ -1,2 +1,2 @@
-rpn -E -a2002 2 1500 range lambda 2 x ** previous_prime 2 x ** - eval 2 enumerate -s1 > powers_of_2_previous_primes.txt
-rpn -E -a5002 1 2050 range lambda 10 x ** previous_prime 10 x ** - eval 1 enumerate -s1 > powers_of_10_previous_primes.txt
+rpn -E -a3002 2 5000 range lambda 2 x ** previous_prime 2 x ** - eval 2 enumerate -s1 > powers_of_2_previous_primes.txt
+rpn -E -a5002 1 4000 range lambda 10 x ** previous_prime 10 x ** - eval 1 enumerate -s1 > powers_of_10_previous_primes.txt
