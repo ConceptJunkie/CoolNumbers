ea53954ed55b6a6191fd8b0a1f96984f482be2b6
Rick Gutleber <rgutlebe@nvrinc.com>
2025-07-23 16:17:55 -0400

    Updating CoolNumbers with some more cool numbers, including all the known Mersenne primes

---
+++ make_10_x_primes.bat
@@ -0,0 +1,31 @@
+rpn -a10010 -E 1[0:0:10000]1 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 1_0_1_primes.txt
+rpn -a10010 -E 1[0:0:10000]3 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 1_0_3_primes.txt
+rpn -a10010 -E 1[0:0:10000]7 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 1_0_7_primes.txt
+rpn -a10010 -E 1[0:0:10000]9 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 1_0_9_primes.txt
+
+rpn -a10010 -E 2[0:0:10000]3 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 2_0_3_primes.txt
+rpn -a10010 -E 2[0:0:10000]9 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 2_0_9_primes.txt
+
+rpn -a10010 -E 3[0:0:10000]1 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 3_0_1_primes.txt
+rpn -a10010 -E 3[0:0:10000]7 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 3_0_7_primes.txt
+
+rpn -a10010 -E 4[0:0:10000]1 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 4_0_1_primes.txt
+rpn -a10010 -E 4[0:0:10000]3 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 4_0_3_primes.txt
+rpn -a10010 -E 4[0:0:10000]7 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 4_0_7_primes.txt
+rpn -a10010 -E 4[0:0:10000]9 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 4_0_7_primes.txt
+
+rpn -a10010 -E 5[0:0:10000]3 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 5_0_3_primes.txt
+rpn -a10010 -E 5[0:0:10000]9 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 5_0_9_primes.txt
+
+rpn -a10010 -E 6[0:0:10000]1 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 6_0_1_primes.txt
+rpn -a10010 -E 6[0:0:10000]7 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 6_0_3_primes.txt
+
+rpn -a10010 -E 7[0:0:10000]1 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 7_0_1_primes.txt
+rpn -a10010 -E 7[0:0:10000]3 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 7_0_3_primes.txt
+rpn -a10010 -E 7[0:0:10000]9 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 7_0_9_primes.txt
+
+rpn -a10010 -E 8[0:0:10000]3 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 8_0_3_primes.txt
+rpn -a10010 -E 8[0:0:10000]9 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 8_0_9_primes.txt
+
+rpn -a10010 -E 9[0:0:10000]1 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 9_0_1_primes.txt
+rpn -a10010 -E 9[0:0:10000]7 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 9_0_7_primes.txt
--- makenext.bat
+++
@@ -1,2 +0,0 @@
-rpn -E -a3010 1 5000 range lambda 2 x ** next_prime 2 x ** - eval 1 enumerate -s1 > powers_of_2_next_primes.txt
-rpn -E -a5010 1 5000 range lambda 10 x ** next_prime 10 x ** - eval 1 enumerate -s1 > powers_of_10_next_primes.txt
