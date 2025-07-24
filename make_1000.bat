924d7826200673b54e3bf68cfa74e59728c6172a
Rick Gutleber <rgutlebe@nvrinc.com>
2025-07-24 13:02:41 -0400

    Some more new primes and some files have better names

---
+++ make_1000.bat
@@ -0,0 +1,5 @@
+rem rpn -t -a1502 [ 2 500 range 1000 repunit 1 999 range ] multiplex lambda x y * 2 + for_each lambda x is_prime filter -s1 | tee 1000_repunit_plus_2.txt
+rpn -t -a1502 [ 2 500 range 1000 repunit 1 999 range ] multiplex lambda x y * 1 + for_each lambda x is_prime filter -s1 | tee 1000_repunit_plus_1.txt
+rpn -t -a1502 [ 2 500 range 1000 repunit 1 999 range ] multiplex lambda x y * 1 - for_each lambda x is_prime filter -s1 | tee 1000_repunit_minus_1.txt
+rpn -t -a1502 [ 2 500 range 1000 repunit 1 999 range ] multiplex lambda x y * 2 - for_each lambda x is_prime filter -s1 | tee 1000_repunit_minus_2.txt
+
--- makerepunit.bat
+++
@@ -1,2 +0,0 @@
-rpn -a652 -E 1 8 range lambda 1 650 range 10 repunit x * next_prime eval -s1 flatten sort > repunit_next.txt
-rpn -a652 -E 1 9 range lambda 2 650 range 10 repunit x * previous_prime eval -s1 flatten sort > repunit_prev.txt
