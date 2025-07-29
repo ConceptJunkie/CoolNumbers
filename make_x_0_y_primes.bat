f20c3dbb957882a98f6e236f629e4ec790998799
Rick Gutleber <rgutlebe@nvrinc.com>
2025-07-29 08:38:46 -0400

    finished the x_0_y primes

--- make_x_0_y_primes.bat
+++ make_x_0_y_primes.bat
@@ -18,7 +18,7 @@ rpn -a10010 -E 5[0:0:10000]3 build_numbers 0 enumerate lambda x 1 element is_pri
 rpn -a10010 -E 5[0:0:10000]9 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 5_0_9_primes.txt
 
 rpn -a10010 -E 6[0:0:10000]1 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 6_0_1_primes.txt
-rpn -a10010 -E 6[0:0:10000]7 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 6_0_3_primes.txt
+rpn -a10010 -E 6[0:0:10000]7 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 6_0_7_primes.txt
 
 rpn -a10010 -E 7[0:0:10000]1 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 7_0_1_primes.txt
 rpn -a10010 -E 7[0:0:10000]3 build_numbers 0 enumerate lambda x 1 element is_prime filter -s1 -t > 7_0_3_primes.txt
